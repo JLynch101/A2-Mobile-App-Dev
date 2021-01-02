53647ca41ae55f6cffe2df5419663b168a8afb59
Joe Lynch <joelynch101@hotmail.com>
2021-01-02 10:51:16 +0000

    Edited MainApp

--- app/src/main/java/org/wit/financeapp/main/MainApp.kt
+++ app/src/main/java/org/wit/financeapp/main/MainApp.kt
@@ -1,4 +1,17 @@
-package org.wit.financeapp.main
+package org.wit.finance.main
 
-class MainApp {
+import android.app.Application
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import org.wit.finance.models.FinanceMemStore
+
+class MainApp : Application(), AnkoLogger {
+
+    val finances = FinanceMemStore()
+
+    override fun onCreate() {
+        super.onCreate()
+        info("Finance started")
+    }
 }
+
