71826043807328cefda4c6dc14642a7cb2b4d23b
Joe Lynch <joelynch101@hotmail.com>
2021-01-02 00:07:58 +0000

    Edited Title

--- app/src/main/java/org/wit/financeapp/FinanceActivity.kt
+++ app/src/main/java/org/wit/financeapp/FinanceActivity.kt
@@ -2,9 +2,12 @@ package org.wit.financeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class FinanceActivity : AppCompatActivity() {
+class FinanceActivity : AppCompatActivity(), AnkoLogger {
     override fun onCreate(savedInstanceState: Bundle?) {
+        info("Finance Activity started..")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finance)
     }
