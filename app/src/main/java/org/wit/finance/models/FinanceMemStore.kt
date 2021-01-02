919f480034693b20cbb08c5a3dbb5a394dd9ebe6
Joe Lynch <joelynch101@hotmail.com>
2021-01-02 11:14:47 +0000

    Added models

---
+++ app/src/main/java/org/wit/finance/models/FinanceMemStore.kt
@@ -0,0 +1,40 @@
+package org.wit.finance.models
+
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+
+var lastId = 0L
+
+internal fun getId(): Long {
+    return lastId++
+}
+
+class FinanceMemStore : FinanceStore, AnkoLogger {
+
+    val finances = ArrayList<FinanceModel>()
+
+    override fun findAll(): List<FinanceModel> {
+        return finances
+    }
+
+    override fun create(finance: FinanceModel) {
+        finance.id = getId()
+        finances.add(finance)
+        logAll()
+    }
+
+    override fun update(finance: FinanceModel) {
+        var foundFinance: FinanceModel? = finances.find { p -> p.id == finance.id }
+        if (foundFinance != null) {
+            foundFinance.title = finance.title
+            foundFinance.description = finance.description
+            foundFinance.image = finance.image
+            logAll();
+        }
+    }
+
+    fun logAll() {
+        finances.forEach { info("${it}") }
+    }
+}
+
---
+++ app/src/main/java/org/wit/finance/models/FinanceStore.kt
@@ -0,0 +1,7 @@
+package org.wit.finance.models
+
+interface FinanceStore {
+    fun findAll(): List<FinanceModel>
+    fun create(finance: FinanceModel)
+    fun update(finance: FinanceModel)
+}
