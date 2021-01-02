001e5cbdf5d892629705df11992ffeb809a90945
Joe Lynch <joelynch101@hotmail.com>
2021-01-02 10:49:14 +0000

    Added FinanceListActvity

--- app/src/main/java/org/wit/financeapp/activities/FinanceListActivity.kt
+++ app/src/main/java/org/wit/financeapp/activities/FinanceListActivity.kt
@@ -1,4 +1,54 @@
-package org.wit.financeapp.activities
+package org.wit.finance.activities
 
-class FinanceListActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_finance_list.*
+import kotlinx.android.synthetic.main.card_finance.view.*
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.startActivityForResult
+import org.wit.finance.main.MainApp
+import org.wit.finance.models.FinanceModel
+import org.wit.financeapp.R
+
+class FinanceListActivity : AppCompatActivity(), FinanceListener {
+
+    lateinit var app: MainApp
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finance_list)
+        app = application as MainApp
+        toolbar.title = title
+        setSupportActionBar(toolbar)
+
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = FinanceAdapter(app.finances.findAll(), this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_add -> startActivityForResult<FinanceActivity>(0)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onFinanceClick(finance: FinanceModel) {
+        startActivityForResult(intentFor<FinanceActivity>().putExtra("finance_edit", finance), 0)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        recyclerView.adapter?.notifyDataSetChanged()
+        super.onActivityResult(requestCode, resultCode, data)
+    }
 }
+
