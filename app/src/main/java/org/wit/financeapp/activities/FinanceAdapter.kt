152bb4240603aa199190a3311fcdcbecdd83dca0
Joe Lynch <joelynch101@hotmail.com>
2021-01-02 10:48:10 +0000

    Added FinanceAdapter

---
+++ app/src/main/java/org/wit/financeapp/activities/FinanceAdapter.kt
@@ -0,0 +1,40 @@
+package org.wit.finance.activities
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.card_finance.view.*
+import org.wit.finance.helpers.readImageFromPath
+import org.wit.finance.models.FinanceModel
+import org.wit.financeapp.R
+
+interface FinanceListener {
+    fun onFinanceClick(finance: FinanceModel)
+}
+
+class FinanceAdapter constructor(private var finances: List<FinanceModel>,
+                                 private val listener: FinanceListener) : RecyclerView.Adapter<FinanceAdapter.MainHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        return MainHolder(LayoutInflater.from(parent.context).inflate(R.layout.card_finance, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+        val finance = finances[holder.adapterPosition]
+        holder.bind(finance, listener)
+    }
+
+    override fun getItemCount(): Int = finances.size
+
+    class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(finance: FinanceModel, listener: FinanceListener) {
+            itemView.financeTitle.text = finance.title
+            itemView.description.text = finance.description
+            itemView.imageIcon.setImageBitmap(readImageFromPath(itemView.context, finance.image))
+            itemView.setOnClickListener { listener.onFinanceClick(finance) }
+        }
+    }
+}
+
